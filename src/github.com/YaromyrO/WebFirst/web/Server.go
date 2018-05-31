da30fb6009a6cf1a63c6715a2ca3b1ab3e2243f5
Yaromyr <oryshchyn97@gmail.com>
2018-05-31 22:02:58 +0300

    changes

--- src/github.com/YaromyrO/WebFirst/web/Server.go
+++ src/github.com/YaromyrO/WebFirst/web/Server.go
@@ -1,8 +1,11 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+)
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("./html")))
-	http.ListenAndServe(":3000", nil)
+	http.Handle("/", http.FileServer(http.Dir("src/github.com/YaromyrO/WebFirst/html")))
+	http.ListenAndServe(":8000", nil)
 }
+
